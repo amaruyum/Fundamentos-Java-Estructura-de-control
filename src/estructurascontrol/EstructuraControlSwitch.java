cc7f6425cf0667ed99d39db2442a72b98e798707
Amaruyum <amaruyum@gmail.com>
2022-03-02 09:09:02 -0500

    Se agregos una nueva clase y un Ejercicio

--- src/estructurascontrol/EstructuraControlSwitch.java
+++ src/estructurascontrol/EstructuraControlSwitch.java
@@ -1,14 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package estructurascontrol;
 
-/**
- *
- * @author SUPPORT
- */
 public class EstructuraControlSwitch {
-    
+
+    public static void main(String args[]) {
+        var numero = 2;
+        var numeroTexto = "Valor desconocido";
+        
+        switch(numero){
+            case 1:
+                numeroTexto = "Numero uno";
+                break;
+            case 2:
+                numeroTexto = "Numero dos";
+                break;
+            case 3:
+                numeroTexto = "Numero tres";
+                break;
+            case 4:
+                numeroTexto = "Numero cuatro";
+                break;
+            default:
+                numeroTexto = "Caso no encontrado";
+        }
+        System.out.println("numeroTexto = " + numeroTexto);
+    }
 }
